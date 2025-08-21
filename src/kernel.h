ab0f3dee6d46d1adcdc5d0002ef280d787b23350
Gabriel Lee <gabewblee@Gabriels-MacBook-Pro-5.local>
2025-08-21 01:02:42 -0400

    Added constants pertaining to VGA dimensions inside of kernel.h

--- src/kernel.h
+++ src/kernel.h
@@ -1,6 +1,16 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+#include <stdint.h>
+#include <stddef.h>
+
+// Dimensions
+#define WIDTH 80
+#define HEIGHT 20
+
+#define MDA 0xB0000
+#define CGA 0xB8000
+
 void kernelMain();
 
 #endif
